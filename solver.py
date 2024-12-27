6f2e612a0f58fc86d390b448fc8c2d4179073b37
Reed C G Xie <reedcgx@gmail.com>
2024-12-27 20:53:26 +0800

    Update solver.py

--- solver.py
+++ solver.py
@@ -7,14 +7,12 @@ DELTA_CLIP = 50.0
 
 
 class BSDESolver(object):
-    """The fully connected neural network model."""
     def __init__(self, config, bsde):
         self.eqn_config = config.eqn_config
         self.net_config = config.net_config
         self.bsde = bsde
 
         self.model = NonsharedModel(config, bsde)
-        self.y_init = self.model.y_init
         lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
             self.net_config.lr_boundaries, self.net_config.lr_values)
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule, epsilon=1e-8)
@@ -23,25 +21,26 @@ class BSDESolver(object):
         start_time = time.time()
         training_history = []
         valid_data = self.bsde.sample(self.net_config.valid_size)
-
-        # begin sgd iteration
+        
         for step in range(self.net_config.num_iterations+1):
             if step % self.net_config.logging_frequency == 0:
                 loss = self.loss_fn(valid_data, training=False).numpy()
-                y_init = self.y_init.numpy()[0]
+                y_init = self.model.y_init.numpy()[0]
                 elapsed_time = time.time() - start_time
                 training_history.append([step, loss, y_init, elapsed_time])
+                
+                    
                 if self.net_config.verbose:
                     logging.info("step: %5u,    loss: %.4e, Y0: %.4e,   elapsed time: %3u" % (
                         step, loss, y_init, elapsed_time))
             self.train_step(self.bsde.sample(self.net_config.batch_size))
+            
         return np.array(training_history)
 
     def loss_fn(self, inputs, training):
         dw, x = inputs
-        y_terminal = self.model(inputs, training)
+        y_terminal = self.model(inputs, training=training)
         delta = y_terminal - self.bsde.g_tf(self.bsde.total_time, x[:, :, -1])
-        # use linear approximation outside the clipped range
         loss = tf.reduce_mean(tf.where(tf.abs(delta) < DELTA_CLIP, tf.square(delta),
                                        2 * DELTA_CLIP * tf.abs(delta) - DELTA_CLIP ** 2))
 
@@ -56,42 +55,59 @@ class BSDESolver(object):
 
     @tf.function
     def train_step(self, train_data):
-        grad = self.grad(train_data, training=True)
-        self.optimizer.apply_gradients(zip(grad, self.model.trainable_variables))
+        with tf.GradientTape() as tape:
+            loss = self.loss_fn(train_data, training=True)
+        
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+
 
 
 class NonsharedModel(tf.keras.Model):
     def __init__(self, config, bsde):
-        super(NonsharedModel, self).__init__()
+        super().__init__()
         self.eqn_config = config.eqn_config
         self.net_config = config.net_config
         self.bsde = bsde
-        self.y_init = tf.Variable(np.random.uniform(low=self.net_config.y_init_range[0],
-                                                    high=self.net_config.y_init_range[1],
-                                                    size=[1])
-                                  )
-        self.z_init = tf.Variable(np.random.uniform(low=-.1, high=.1,
-                                                    size=[1, self.eqn_config.dim])
-                                  )
+        y_init_range = self.net_config.y_init_range
+        
+        self.y_init = self.add_weight(
+            name='y_init',
+            shape=[1],
+            initializer=tf.random_uniform_initializer(
+                y_init_range[0],
+                y_init_range[1]
+            )
+        )
+        
+        self.z_init = self.add_weight(
+            name='z_init',
+            shape=[1, self.eqn_config.dim],
+            initializer=tf.random_uniform_initializer(-0.1, 0.1)
+        )
 
         self.subnet = [FeedForwardSubNet(config) for _ in range(self.bsde.num_time_interval-1)]
 
-    def call(self, inputs, training):
+    def call(self, inputs, training=None):
         dw, x = inputs
         time_stamp = np.arange(0, self.eqn_config.num_time_interval) * self.bsde.delta_t
         all_one_vec = tf.ones(shape=tf.stack([tf.shape(dw)[0], 1]), dtype=self.net_config.dtype)
+    
         y = all_one_vec * self.y_init
         z = tf.matmul(all_one_vec, self.z_init)
-
+    
         for t in range(0, self.bsde.num_time_interval-1):
             y = y - self.bsde.delta_t * (
                 self.bsde.f_tf(time_stamp[t], x[:, :, t], y, z)
             ) + tf.reduce_sum(z * dw[:, :, t], 1, keepdims=True)
-            z = self.subnet[t](x[:, :, t + 1], training) / self.bsde.dim
+
+            z = self.subnet[t](x[:, :, t + 1], training=training) / self.bsde.dim
+
         # terminal time
         y = y - self.bsde.delta_t * self.bsde.f_tf(time_stamp[-1], x[:, :, -2], y, z) + \
             tf.reduce_sum(z * dw[:, :, -1], 1, keepdims=True)
-
+    
         return y
 
 
@@ -100,6 +116,8 @@ class FeedForwardSubNet(tf.keras.Model):
         super(FeedForwardSubNet, self).__init__()
         dim = config.eqn_config.dim
         num_hiddens = config.net_config.num_hiddens
+
+
         self.bn_layers = [
             tf.keras.layers.BatchNormalization(
                 momentum=0.99,
@@ -112,16 +130,17 @@ class FeedForwardSubNet(tf.keras.Model):
                                                    use_bias=False,
                                                    activation=None)
                              for i in range(len(num_hiddens))]
-        # final output should be gradient of size dim
         self.dense_layers.append(tf.keras.layers.Dense(dim, activation=None))
+ 
 
-    def call(self, x, training):
+    def call(self, x, training=None):
         """structure: bn -> (dense -> bn -> relu) * len(num_hiddens) -> dense -> bn"""
-        x = self.bn_layers[0](x, training)
+        x = self.bn_layers[0](x, training=training)
         for i in range(len(self.dense_layers) - 1):
             x = self.dense_layers[i](x)
-            x = self.bn_layers[i+1](x, training)
+            x = self.bn_layers[i+1](x, training=training)
             x = tf.nn.relu(x)
         x = self.dense_layers[-1](x)
-        x = self.bn_layers[-1](x, training)
+        x = self.bn_layers[-1](x, training=training)
+
         return x
