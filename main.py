dde3a7cdea84ac50a67ca32f4d42e0bb7fafaa9c
Reed C G Xie <reedcgx@gmail.com>
2024-12-27 20:52:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,25 +28,45 @@ FLAGS.log_dir = './logs'  # directory where to write event logs and output array
 
 def main(argv):
     del argv
+    tf.keras.backend.clear_session()
     with open(FLAGS.config_path) as json_data_file:
-        config = json.load(json_data_file)
-    config = munch.munchify(config)
-    bsde = getattr(eqn, config.eqn_config.eqn_name)(config.eqn_config)
-    tf.keras.backend.set_floatx(config.net_config.dtype)
+        config_dict = json.load(json_data_file)
+    class DictToObject:
+        def __init__(self, dictionary):
+            self._dict = dictionary
+            for key, value in dictionary.items():
+                setattr(self, key, value)
+                
+        def to_dict(self):
+            return self._dict
+    
+    class Config:
+        def __init__(self, config_dict):
+            self.eqn_config = DictToObject(config_dict['eqn_config'])
+            self.net_config = DictToObject(config_dict['net_config'])
+            self._original_dict = config_dict
+            
+        def to_dict(self):
+            return self._original_dict
+    
+    config = Config(config_dict)
 
+    tf.keras.backend.set_floatx(config.net_config.dtype)
+    
+
+    bsde = getattr(eqn, config.eqn_config.eqn_name)(config.eqn_config)
+    bsde_solver = BSDESolver(config, bsde)
+    
     if not os.path.exists(FLAGS.log_dir):
         os.mkdir(FLAGS.log_dir)
     path_prefix = os.path.join(FLAGS.log_dir, FLAGS.exp_name)
     with open('{}_config.json'.format(path_prefix), 'w') as outfile:
-        json.dump(dict((name, getattr(config, name))
-                       for name in dir(config) if not name.startswith('__')),
-                  outfile, indent=2)
+        json.dump(config.to_dict(), outfile, indent=2)
 
     absl_logging.get_absl_handler().setFormatter(logging.Formatter('%(levelname)-6s %(message)s'))
     absl_logging.set_verbosity('info')
 
     logging.info('Begin to solve %s ' % config.eqn_config.eqn_name)
-    bsde_solver = BSDESolver(config, bsde)
     training_history = bsde_solver.train()
     if bsde.y_init:
         logging.info('Y0_true: %.4e' % bsde.y_init)
@@ -59,6 +79,6 @@ def main(argv):
                header='step,loss_function,target_value,elapsed_time',
                comments='')
 
-
 if __name__ == '__main__':
+
     app.run(main)
